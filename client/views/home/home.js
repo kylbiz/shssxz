bd96ba289fa4eb426a17f0d3090e8abc032fd41a
ZZR-china <2467254599@qq.com>
2016-07-14 18:21:52 +0800

    today aaa we are young

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,11 +1,21 @@
+Template.home.onRendered(function () {
+	$(document).ready(function($) {
+				var slider = $('.my-slider').unslider({
+					keys: {
+						prev: 37,
+						next: 39,
+						stop: 27 //  Example: pause when the Esc key is hit
+					},
+					arrows: {
+						//  Unslider default behaviour
+						prev: '<a class="unslider-arrow prevh"><i class="fa fa-angle-left fa-5x" aria-hidden="true"></i></a>',
+						next: '<a class="unslider-arrow nexth"><i class="fa fa-angle-right fa-5x" aria-hidden="true"></i></a>',
+					},
+				});
+				// slider.on();
+	});
+});
+
 Template.home.helpers({
-	news: function() {
-		return [
-		 	{
-		 		title: '',
-		 		date: '',
-		 		url: ''
-		 	},
-		 ]
-	},
+	
 });
