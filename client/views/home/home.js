ab5d8ea7c4a7cc03149f5118b9e394efafcb69a4
ZZR-china <2467254599@qq.com>
2016-07-15 19:13:43 +0800

    home status

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,21 +1,109 @@
 Template.home.onRendered(function () {
 	$(document).ready(function($) {
-				var slider = $('.my-slider').unslider({
-					keys: {
-						prev: 37,
-						next: 39,
-						stop: 27 //  Example: pause when the Esc key is hit
-					},
-					arrows: {
-						//  Unslider default behaviour
-						prev: '<a class="unslider-arrow prevh"><i class="fa fa-angle-left fa-5x" aria-hidden="true"></i></a>',
-						next: '<a class="unslider-arrow nexth"><i class="fa fa-angle-right fa-5x" aria-hidden="true"></i></a>',
-					},
-				});
-				// slider.on();
+		var slider = $('.my-slider').unslider({
+			keys: {
+				prev: 37,
+				next: 39,
+				stop: 27 //  Example: pause when the Esc key is hit
+			},
+			arrows: {
+				//  Unslider default behaviour
+				prev: '<a class="unslider-arrow prevh"><i class="fa fa-angle-left fa-5x" aria-hidden="true"></i></a>',
+				next: '<a class="unslider-arrow nexth"><i class="fa fa-angle-right fa-5x" aria-hidden="true"></i></a>',
+			},
+		});
+		$('.news .content-title span').click(function (e) {
+			var target = e.currentTarget;
+			var value = $(target).attr('val');
+			Session.set('news',value);
+			console.log(Session.get('news'));
+		});
 	});
 });
 
 Template.home.helpers({
-	
+	news:function(){
+		return [
+			{
+				text:'APEC商务签证服务',
+				time:'2011-01-12'
+			},
+			{
+				text:'APEC商务签证服务',
+				time:'2011-01-12'
+			},
+			{
+				text:'APEC商务签证服务',
+				time:'2011-01-12'
+			},
+			{
+				text:'APEC商务签证服务',
+				time:'2011-01-12'
+			}
+		]
+	},
+	notice:function(){
+		return [
+			{
+				text:'APEC11111商务签证服务',
+				time:'2011-01-12'
+			},
+			{
+				text:'APEeeeeC商务签证服务',
+				time:'2011-01-12'
+			},
+			{
+				text:'APEC商eee务签证服务',
+				time:'2011-01-12'
+			},
+			{
+				text:'APEC商务eee签证服务',
+				time:'2011-01-12'
+			}
+		]
+	},
+	imgContent:function(){
+		return [
+			{
+				url:'/image/home/cowin/dehao.jpg',
+				text:'上海德皓有限公司',
+			},
+			{
+				url:'/image/home/cowin/meilinge.jpg',
+				text:'上海美林阁餐饮经营管理有限公司',
+			},
+			{
+				url:'/image/home/cowin/diyang.jpg',
+				text:'上海迪洋服饰有限公司',
+			},
+			{
+				url:'/image/home/cowin/chuangyuan.jpg',
+				text:'上海创远信息有限公司',
+			},
+
+		]
+	},
+	noticeNews:function(){
+		return Session.get('news')||'news';
+	},
+	serviceimg:function(){
+		return [
+			{
+				url:'/image/home/service/overview.jpg',
+				imgtext:'园区概况'
+			},
+			{
+				url:'/image/home/service/env.jpg',
+				imgtext:'投资环境'
+			},
+			{
+				url:'/image/home/service/service.jpg',
+				imgtext:'投资服务'
+			},
+			{
+				url:'/image/home/service/step.jpg',
+				imgtext:'办理流程'
+			},
+		]
+	}
 });
